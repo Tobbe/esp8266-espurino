8077d735adfbc35e52053dc50fe5538bfe5d0070
Neil Kolban <kolban1@kolban.com>
2015-08-23 10:18:42 -0500

    First build of an ELF binary.

---
+++ espruino/targets/esp8266/user_config.h
@@ -0,0 +1,13 @@
+/*
+ * user_config.h
+ *
+ *  Created on: Aug 23, 2015
+ *      Author: kolban
+ */
+
+#ifndef TARGETS_ESP8266_USER_CONFIG_H_
+#define TARGETS_ESP8266_USER_CONFIG_H_
+
+
+
+#endif /* TARGETS_ESP8266_USER_CONFIG_H_ */
---
+++ espruino/targets/esp8266/user_main.c
@@ -0,0 +1,7 @@
+void user_rf_pre_init() {
+
+}
+
+void user_init() {
+
+}
